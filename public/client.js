d27be2f19c399acf71785a1e917feabb5435059c
Glitch (watson-personality-api) <none>
2018-04-29 04:45:06 +0000

    Add if/else color condition based on label to server.js to eventually moved elsewhere.

--- public/client.js
+++ public/client.js
@@ -10,27 +10,22 @@
 var thePersonality = ([
   {
     label: "Openness",
-    color: "#FF9901", // warm orange 
     value: 0.9970814244982862
   },
   {
     label: "Conscientiousness",
-    color: "#52154E", // purple
     value: 0.986401677449357
   },
   {
     label: "Extraversion",
-    color: "#00A896", // open sea green
     value: 0.08530058556548387
   },
   {
     label: "Agreeableness",
-    color: "#8E1C3E", // easy mauve
     value: 0.18753528603194114
   },
   {
     label: "Emotional range",
-    color: "#FF3333", // fiery orange
     value: 0.9438564164580441
   }
 ]); 
@@ -44,8 +39,16 @@ svg.selectAll("circle")
   .enter().append("circle")      
     .style("stroke", "#A997DF")
     .style("stroke-width", "2")
-    .attr("r", function(d) { return d.value * 100 })
-    .style('fill', function(d) { return d.color })
+    .attr("r", function(d) { return d.value * 100 })    
+    .style('fill', function(d) { 
+      // OPTIMIZE move into pTraits.js and add other traits and colors
+      if (d.label === "Openness") { return "#FF9901" } // warm orange 
+      else if (d.label === "Conscientiousness") { return "#52154E" } // noble purple
+      else if (d. label === "Extraversion") { return "#00A896" } // open sea green
+      else if (d. label === "Agreeableness") { return "#8E1C3E" } // easy mauve
+      else if (d. label === "Emotional range") { return "#FF3333" } // fiery orange
+      else { return "#111344" } // deep blue
+    })
     .attr("cx", function() { return Math.random() * 500; })
     .attr("cy", function() { return Math.random() * 500; })
     
